fa95507b6bc2a19076bc3b56cdc6111f55f82a85
Rafale25 <rafale25.dev@gmail.com>
2026-08-25 00:33:28 +0200

    update

--- src/main.js
+++ src/main.js
@@ -1,14 +1,8 @@
 import './style.css'
-import javascriptLogo from './assets/javascript.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import { router } from "./router.js";
 
 document.querySelector('#app').innerHTML = `
-<body>
-  Hello
-</body>
-<div id="app"/>
+<h1>Hello</h1>
 `
 
 router()
